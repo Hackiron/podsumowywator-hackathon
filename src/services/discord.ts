06e7cb13d90c4bf29af2708cb4dcc9bffcc0448a
saitamason <92246829+saitamason@users.noreply.github.com>
2025-04-25 21:35:09 +0200

    feat: disable 'fasada' openai and add 'npm run dev' command

--- src/services/discord.ts
+++ src/services/discord.ts
@@ -1,33 +1,33 @@
 import dotenv from "dotenv";
-import { Message, OpenAi } from "./openai.js";
-import { ContextService } from "./context.js";
+// import { Message, OpenAi } from "./openai.js";
+import { DiscordHelper } from "../helpers/discord.js";
 import { exceptionHandler } from "../utils/helpers.js";
 import { ClientService } from "./client.js";
-import { DiscordHelper } from "../helpers/discord.js";
+import { ContextService } from "./context.js";
 
 dotenv.config();
 const { DISCORD_CLIENT_TOKEN, CHANNEL_ID, PODSUMOWYWATOR_ID } = process.env;
 
-const openai = new OpenAi();
+// const openai = new OpenAi();
 
 export class DiscordService {
   private client: any;
-  private systemContext: Message;
+  // private systemContext: Message;
   constructor() {
     const contextService = new ContextService([]);
     const clientService = new ClientService();
     this.client = clientService.getClient();
     const discordHelper = new DiscordHelper(this.client);
 
-    this.systemContext = {
-      role: "system",
-      content:
-        "Podsumuj Podane wiadomości. Z podanych wiadomości wyciągnij to o czym  była dyskusja i podsumuj. Podsumuj krótko zwięźle i na temat.",
-    };
+    // this.systemContext = {
+    //   role: "system",
+    //   content:
+    //     "Podsumuj Podane wiadomości. Z podanych wiadomości wyciągnij to o czym  była dyskusja i podsumuj. Podsumuj krótko zwięźle i na temat.",
+    // };
 
     this.client.on("ready", async () => {
       const channel = this.client.channels.cache.get(CHANNEL_ID);
-      
+
       // TESTOWE POBIERANIE WIADOMOŚCI
       const messages = await discordHelper.fetchMessages(
         CHANNEL_ID,
@@ -35,11 +35,14 @@ export class DiscordService {
         "2025-04-14T00:00:00Z"
       );
 
-      console.log("messages: ", messages.map((msg: any) => msg.content));
+      console.log(
+        "messages: ",
+        messages.map((msg: any) => msg.content)
+      );
 
       try {
         console.log(`Logged in as ${this.client.user.tag}!`);
-        channel.send(`Dzień dobry, jestem gotowy do podsumowywania!`);
+        channel.send(`Elo, jestem gotowy do podsumowywania!`);
       } catch (error: any) {
         return exceptionHandler(error, channel);
       }
@@ -58,15 +61,16 @@ export class DiscordService {
         if (message.author.username !== "Podsumowywator") {
           try {
             message.channel.sendTyping();
-            const assResponse = await openai.contextInteract([
-              this.systemContext,
-              ...contextService.getContext(),
-            ]);
+            // const assResponse = await openai.contextInteract([
+            //   this.systemContext,
+            //   ...contextService.getContext(),
+            // ]);
 
-            assResponse && contextService.pushWithLimit(assResponse);
+            // assResponse && contextService.pushWithLimit(assResponse);
 
-            const responseContent = `${assResponse.content.substring(0, 1950)}`;
-            message.reply(responseContent);
+            // const responseContent = `${assResponse.content.substring(0, 1950)}`;
+            // message.reply(responseContent);
+            message.reply("Działam!");
           } catch (error: any) {
             return exceptionHandler(error, message);
           }
@@ -79,7 +83,7 @@ export class DiscordService {
 
   userResponseFactory(message: any) {
     const modifiedUserResponseContent = `${message.author.globalName}: ${message.content}`;
-    return openai.messageFactory(modifiedUserResponseContent);
+    // return openai.messageFactory(modifiedUserResponseContent);
   }
 
   destroy() {
