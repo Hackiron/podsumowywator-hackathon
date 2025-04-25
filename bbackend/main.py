f03c8a2ea23d65bfb606bd60f5b52df2ee622230
Dombear <domin.dober@gmail.com>
2025-04-25 21:07:24 +0200

    Add format command

--- bbackend/main.py
+++ bbackend/main.py
@@ -2,10 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI(title="Podsumowywator Hackathon Bbackend API")
 
+
 @app.get("/")
 async def root():
     return {"message": "Welcome to the Podsumowywator Hackathon Bbackend API"}
 
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000) 
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
